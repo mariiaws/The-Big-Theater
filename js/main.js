219a8babaeeca9d5920b27f59435b1aab5f87829
mariiaws <wooglay@gmail.com>
2022-09-12 17:39:41 +0300

    Created all a hamburger.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-
-
 "use strict"
 
+let animateLinesHamburger = document.querySelector(".hamburger").addEventListener("click", function () {
 
-let click = document.querySelector(".hamburger").addEventListener("click", function () {
-    document.querySelector(".line-top").classList.toggle("opened");
-  document.querySelector(".line-middle").classList.toggle("opened");
-    document.querySelector(".line-bottom").classList.toggle("opened");
-    this.setAttribute('aria-expanded',
-        this.classList.contains('opened'));
+    document.querySelector(".nav").classList.toggle("nav-open")
+    document.querySelector(".hamburger").classList.toggle("button-hamburger")
+    document.querySelector(".line-top").classList.toggle("line-top-animate");
+    document.querySelector(".line-middle").classList.toggle("line-middle-animate");
+    document.querySelector(".line-bottom").classList.toggle("line-bottom-animate");
+
+});
+
+
+let openHamburger = document.querySelector(".hamburger");
+openHamburger.addEventListener("click", function () {
+
+    document.querySelector(".hamburger-wrapper").classList.toggle("hamburger-open");
+
 });
 
 
