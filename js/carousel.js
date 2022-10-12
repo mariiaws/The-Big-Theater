034e95d37f58588fff2745ceb7b6bf3be28cdaa6
mariiaws <wooglay@gmail.com>
2022-10-12 16:11:52 +0300

    Finished the hover loupe for the carousel

--- js/carousel.js
+++ js/carousel.js
@@ -42,5 +42,60 @@ carousel.onmousedown = function (event) {
 
 };
 
+function hoverLoupe() {
+    let backgroundLoupeOne = document.querySelector(".background-loupe-one");
+    let backgroundLoupeTwo = document.querySelector(".background-loupe-two");
+    let backgroundLoupeThree = document.querySelector(".background-loupe-three");
+    let backgroundLoupeFour = document.querySelector(".background-loupe-four");
+
+    let loupe = document.querySelectorAll(".loupe-one, .loupe-two, .loupe-three, .loupe-four");
+    let loupeOne = document.querySelector(".loupe-one");
+    let loupeTwo = document.querySelector(".loupe-two");
+    let loupeThree = document.querySelector(".loupe-three");
+    let loupeFour = document.querySelector(".loupe-four");
 
 
+    loupe.forEach(item => {
+        item.style.display = "none";
+    })
+
+    function showLoupe() {
+        backgroundLoupeOne.onmouseover = function () {
+            loupeOne.style.display = "flex";
+        }
+
+        backgroundLoupeTwo.onmouseover = function () {
+            loupeTwo.style.display = "flex";
+        }
+
+        backgroundLoupeThree.onmouseover = function () {
+            loupeThree.style.display = "flex";
+        }
+
+        backgroundLoupeFour.onmouseover = function () {
+            loupeFour.style.display = "flex";
+        }
+    }
+    showLoupe();
+
+    function hideLoupe() {
+        backgroundLoupeOne.onmouseout = function () {
+            loupeOne.style.display = "none"
+        }
+
+        backgroundLoupeTwo.onmouseout = function () {
+            loupeTwo.style.display = "none"
+        }
+
+        backgroundLoupeThree.onmouseout = function () {
+            loupeThree.style.display = "none"
+        }
+
+        backgroundLoupeFour.onmouseout = function () {
+            loupeFour.style.display = "none"
+        }
+    }
+    hideLoupe();
+}
+
+hoverLoupe();
