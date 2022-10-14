0560b4405545c22cc2952fa7ac5e85dc5231461d
mariiaws <wooglay@gmail.com>
2022-10-14 18:48:38 +0300

    Finished the carousel and modal window for desktop

--- js/carousel.js
+++ js/carousel.js
@@ -8,7 +8,7 @@ carousel.onmousedown = function (event) {
 
     moveAt(event.pageX);
 
-    // переносит мяч на координаты (pageX),
+    // переносит карусель на координаты (pageX),
     // дополнительно учитывая изначальный сдвиг относительно указателя мыши
     function moveAt(pageX) {
         carousel.style.left = pageX - shiftX + 'px';
@@ -18,10 +18,10 @@ carousel.onmousedown = function (event) {
         moveAt(event.pageX);
     }
 
-    // передвигаем мяч при событии mousemove
+    // передвигаем карусель при событии mousemove
     document.addEventListener('mousemove', onMouseMove);
 
-    // отпустить мяч, удалить ненужные обработчики
+    // отпустить карусель, удалить ненужные обработчики
     carousel.onmouseup = function () {
         document.removeEventListener('mousemove', onMouseMove);
         carousel.onmouseup = null;
@@ -42,16 +42,24 @@ carousel.onmousedown = function (event) {
 
 };
 
+
 let backgroundLoupeOne = document.querySelector(".background-loupe-one");
 let backgroundLoupeTwo = document.querySelector(".background-loupe-two");
 let backgroundLoupeThree = document.querySelector(".background-loupe-three");
 let backgroundLoupeFour = document.querySelector(".background-loupe-four");
+
+let loupeTextOne = document.querySelector(".loupe-text-one");
+let loupeTextTwo = document.querySelector(".loupe-text-two");
+let loupeTextThree = document.querySelector(".loupe-text-three");
+let loupeTextFour = document.querySelector(".loupe-text-four");
 
 let loupe = document.querySelectorAll(".loupe-one, .loupe-two, .loupe-three, .loupe-four");
 let loupeOne = document.querySelector(".loupe-one");
 let loupeTwo = document.querySelector(".loupe-two");
 let loupeThree = document.querySelector(".loupe-three");
 let loupeFour = document.querySelector(".loupe-four");
+
+let scrollNo;
 
 function hoverLoupe() {
 
@@ -62,18 +70,25 @@ function hoverLoupe() {
     function showLoupe() {
         backgroundLoupeOne.onmouseover = function () {
             loupeOne.style.display = "flex";
+            loupeTextOne.style.opacity = "0"
         }
 
         backgroundLoupeTwo.onmouseover = function () {
             loupeTwo.style.display = "flex";
+            loupeTextTwo.style.opacity = "0"
+
         }
 
         backgroundLoupeThree.onmouseover = function () {
             loupeThree.style.display = "flex";
+            loupeTextThree.style.opacity = "0"
+
         }
 
         backgroundLoupeFour.onmouseover = function () {
             loupeFour.style.display = "flex";
+            loupeTextFour.style.opacity = "0"
+
         }
     }
 
@@ -82,51 +97,110 @@ function hoverLoupe() {
     function hideLoupe() {
         backgroundLoupeOne.onmouseout = function () {
             loupeOne.style.display = "none"
+            loupeTextOne.style.opacity = "1"
+
         }
 
         backgroundLoupeTwo.onmouseout = function () {
             loupeTwo.style.display = "none"
+            loupeTextTwo.style.opacity = "1"
+
         }
 
         backgroundLoupeThree.onmouseout = function () {
             loupeThree.style.display = "none"
+            loupeTextThree.style.opacity = "1"
+
         }
 
         backgroundLoupeFour.onmouseout = function () {
             loupeFour.style.display = "none"
+            loupeTextFour.style.opacity = "1"
+
         }
     }
 
     hideLoupe();
+
 }
 
 hoverLoupe();
 
 
-loupeThree.addEventListener("click", function () {
-    console.log("click");
+loupeOne.addEventListener("click", function () {
 
-    function createWindowImg() {
-        let winDialog = document.createElement("div");
-        winDialog.className = "window-dialog";
-        winDialog.innerHTML = " <div class=\"window-background\"></div>\n" +
-            "        <span class=\"button-close\"></span>\n" +
-            "        <div class=\"window-dialog-wrapper\">\n" +
-            "        <img src=\"img/golden-gramophone.png\" class=\"window-image\" height=\"600\" width=\"443\" alt=\"Золотой граммофон\"/>\n" +
-            "        </div>\n" +
-            "    </div>";
+    let winDialog = document.createElement("div");
+    winDialog.className = "window-dialog";
+    winDialog.innerHTML = " <div class=\"window-background\"></div>\n" +
+        " <span class=\"button-close\"></span>\n" +
+        " <div class=\"window-dialog-wrapper\">\n" +
+        "<img src=\"img/oscar-2020.png\" height=\"600\" width=\"443\" class=\"window-image\" alt=\"Оскар 2020\"/>\n" +
+        " </div>\n" +
+        " </div>";
 
-        document.body.append(winDialog);
+    document.body.append(winDialog);
 
-    }
+    let winBack = document.querySelector(".window-dialog");
+    scrollNo = document.body.style.overflowY = "hidden";
 
-    createWindowImg();
+    winBack.addEventListener("click", function () {
+        winDialog.remove();
+        scrollNo = document.body.style.overflowY = "";
+
+    })
 
 })
 
 
-loupeFour.addEventListener("click", function () {
+loupeTwo.addEventListener("click", function () {
 
+    let winDialog = document.createElement("div");
+    winDialog.className = "window-dialog";
+    winDialog.innerHTML = " <div class=\"window-background\"></div>\n" +
+        " <span class=\"button-close\"></span>\n" +
+        " <div class=\"window-dialog-wrapper\">\n" +
+        " <img src=\"img/general-staff.png\" height=\"600\" width=\"443\" class=\"window-image\" alt=\"Здание главного штаба\"/>\n" +
+        " </div>\n" +
+        " </div>";
+
+    document.body.append(winDialog);
+
+    let winBack = document.querySelector(".window-dialog");
+    scrollNo = document.body.style.overflowY = "hidden";
+
+    winBack.addEventListener("click", function () {
+        winDialog.remove();
+        scrollNo = document.body.style.overflowY = "";
+
+    })
+})
+
+loupeThree.addEventListener("click", function () {
+    console.log("click");
+
+    let winDialog = document.createElement("div");
+    winDialog.className = "window-dialog";
+    winDialog.innerHTML = " <div class=\"window-background\"></div>\n" +
+        "        <span class=\"button-close\"></span>\n" +
+        "        <div class=\"window-dialog-wrapper\">\n" +
+        "        <img src=\"img/golden-gramophone.png\" class=\"window-image\" height=\"600\" width=\"443\" alt=\"Золотой граммофон\"/>\n" +
+        "        </div>\n" +
+        "    </div>";
+
+    document.body.append(winDialog);
+
+    let winBack = document.querySelector(".window-dialog");
+    scrollNo = document.body.style.overflowY = "hidden";
+
+    winBack.addEventListener("click", function () {
+        winDialog.remove();
+        scrollNo = document.body.style.overflowY = "";
+
+    })
+})
+
+
+loupeFour.addEventListener("click", function () {
 
     let winDialog = document.createElement("div");
     winDialog.className = "window-dialog";
@@ -140,9 +214,14 @@ loupeFour.addEventListener("click", function () {
     document.body.append(winDialog);
 
     let winBack = document.querySelector(".window-dialog");
-    winBack.addEventListener("click", () => winDialog.remove());
+    scrollNo = document.body.style.overflowY = "hidden";
 
+    winBack.addEventListener("click", function () {
+        winDialog.remove();
+        scrollNo = document.body.style.overflowY = "";
 
+    })
 })
+
 
 
