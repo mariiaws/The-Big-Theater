fc2bae74dd992fb926cf84644775ce0c33aeb5f6
mariiaws <wooglay@gmail.com>
2022-11-11 12:16:43 +0300

    Modified validation

--- js/main.js
+++ js/main.js
@@ -59,103 +59,51 @@ sliderZIndex();
 
 let form = document.forms[0];
 let name = form.elements.name;
-let tell = form.elements.tel;
+let tel = form.elements.tel;
 
 let mail = form.elements.email;
 let btn = form.elements.btn;
 
-function checkName() {
-    name.onchange = function () {
-        let regexp = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u;
+let new_value;
 
-        if (name.value.match(regexp) != null){
-            name.style.border = "";
-        }else name.style.border = "1px solid #ff0000";
+window.addEventListener("DOMContentLoaded", function () {
+    [].forEach.call(document.querySelectorAll(".form-label__input-tel"), function (input) {
+        let keyCode;
 
-    }
+        function mask(event) {
+            event.keyCode && (keyCode = event.keyCode);
 
-}
-
-
-
-function checkTell() {
-    window.addEventListener("DOMContentLoaded", function () {
-        [].forEach.call(document.querySelectorAll(".form-label__input-tel"), function (input) {
-            let keyCode;
-
-            function mask(event) {
-                event.keyCode && (keyCode = event.keyCode);
-                let matrix = "+7 (___)-___-__-__",
-                    i = 0,
-                    def = matrix.replace(/\D/g, ""),
-                    val = this.value.replace(/\D/g, ""),
-                    new_value = matrix.replace(/[_\d]/g, function (a) {
-                        return i < val.length ? val.charAt(i++) || def.charAt(i) : a //ищет и заменяет
-                    });
-                i = new_value.indexOf("_");
-                if (i != -1) { //тут убираются чёрточки у маски
-                    i < 5 && (i = 3);
-                    new_value = new_value.slice(0, i);
-                }
-                let reg = matrix.substr(0, this.value.length).replace(/_+/g, //заменяет циферки на чёрточки
-                    function (a) {
-                        return "\\d{1," + a.length + "}"
-                    }).replace(/[+()]/g, "\\$&");//подставляет циферки в регулярку
-                reg = new RegExp("^" + reg + "$");//создаёт сво регулярку
-                if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-                if (event.type == "blur" && this.value.length < 5) this.value = "";//ничего не ввёл и ушёл с элемента, тогда в поле ввода пусто
-
-                tell.onchange = function () {
-                    if (new_value.length < 18) {
-                        tell.style.border = "1px solid #ff0000";
-
-                    } else tell.style.border = "";
-
-                }
-
-
+            let matrix = "+7 (___)-___-__-__",
+                i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, "");
+            new_value = matrix.replace(/[_\d]/g, function (a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a //ищет и заменяет
+            });
+            i = new_value.indexOf("_");
+            if (i != -1) { //тут убираются чёрточки у маски
+                i < 5 && (i = 3);
+                new_value = new_value.slice(0, i);
             }
+            let reg = matrix.substr(0, this.value.length).replace(/_+/g, //заменяет циферки на чёрточки
+                function (a) {
+                    return "\\d{1," + a.length + "}"
+                }).replace(/[+()]/g, "\\$&");//подставляет циферки в регулярку
+            reg = new RegExp("^" + reg + "$");//создаёт сво регулярку
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+            if (event.type == "blur" && this.value.length < 5) this.value = "";//ничего не ввёл и ушёл с элемента, тогда в поле ввода пусто
 
-
-            input.addEventListener("input", mask, false);
-            input.addEventListener("focus", mask, false);
-            input.addEventListener("blur", mask, false);
-            input.addEventListener("keydown", mask, false)
-
-        });
-
-    });
-
-}
-
-
-
-function checkMail() {
-    mail.onchange = function () {
-        let regexp = /[-.\w]+@([\w-]+\.)+[\w-]+/g;
-        if (regexp.test(mail.value) == true) {
-            mail.style.border = "";
-        } else {
-            mail.style.border = "1px solid #ff0000";
 
         }
 
-    }
-}
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false)
 
-checkName()
-checkTell()
-checkMail()
+    });
 
-
-btn.addEventListener("click", (event) => event.preventDefault());
-
-/*btn.addEventListener("click", function (event) {
-
-
-})*/
-
-
+});
 
 form.keydown = function (event) {
     if (event.keyCode == 13) {
@@ -163,5 +111,48 @@ form.keydown = function (event) {
         return false;
     }
 }
+
+btn.addEventListener("click", function (send) {
+
+    function checkName() {
+        let regexp = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u;
+        if (name.value.match(regexp) != null) {
+            name.style.border = "";
+        } else {
+            name.style.border = "1px solid #ff0000";
+            send.preventDefault();
+
+        }
+    }
+
+    function checkTel() {
+        if (new_value.length < 18) {
+            tel.style.border = "1px solid #ff0000";
+            send.preventDefault()
+
+        } else {
+            tel.style.border = "";
+        }
+    }
+
+    function checkMail() {
+        let regexp = /[-.\w]+@([\w-]+\.)+[\w-]+/g;
+        if (regexp.test(mail.value) == true) {
+            mail.style.border = "";
+        } else {
+            mail.style.border = "1px solid #ff0000";
+            send.preventDefault()
+
+        }
+
+    }
+
+    checkName()
+    checkMail()
+    checkTel()
+
+})
+
+
 
 
